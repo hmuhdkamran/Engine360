b4a13c5644b9415bc66dc21acc38e3b4a4f1a1b9
H.Muhammad Kamran <hmuhdkamran@gmail.com>
2020-09-07 11:39:39 +0500

    Authentication Logout update

--- Frontend/src/views/services/auth/authentication-service.ts
+++ Frontend/src/views/services/auth/authentication-service.ts
@@ -1,26 +1,45 @@
 import { StoreService } from '@/system/services';
 import { Store } from 'vuex';
 import { TokenHelper, GlobalConfig } from '@/system/config';
-import { ICredential, IAccessToken, IUser } from '@/system/models';
-import Axios from 'axios';
+import { ICredential, IAccessToken, IUser, IPayload, PayloadMessageTypes, DefaultUser } from '@/system/models';
+import Axios, { AxiosResponse } from 'axios';
 
 export class AuthenticationService extends StoreService {
 
-    constructor(store: Store<{}>) {
-        super(store);
-      }
+  constructor(store: Store<{}>) {
+    super(store);
+  }
 
-      login(credentials: ICredential) {
-        let onSuccess = (token: IAccessToken) => {
-          if (token) {
-            TokenHelper.setAccessToken(token.access_token);
-    
-            return TokenHelper.parseUserToken(token.access_token);
-          }
-        };
-    
-        return this.exec<IAccessToken>(Axios.post(GlobalConfig.uri.auth + 'login', credentials))
-          .then(value => this.processPayload(value))
-          .then(onSuccess);
+  login(credentials: ICredential) {
+    let onSuccess = (token: IAccessToken) => {
+      if (token) {
+        TokenHelper.setAccessToken(token.access_token);
+
+        return TokenHelper.parseUserToken(token.access_token);
       }
+    };
+
+    return this.exec<IAccessToken>(Axios.post(GlobalConfig.uri.auth + 'login', credentials))
+      .then(value => this.processPayload(value))
+      .then(onSuccess);
+  }
+
+  logout() {
+    let onSuccess = (res: AxiosResponse) => {
+      let payload: IPayload<IAccessToken> = res.data;
+
+      if (payload.message.messageTypeId === PayloadMessageTypes.success) {
+        let user: IUser = Object.assign({}, DefaultUser);
+
+        TokenHelper.removeAccessToken();
+        window.localStorage.removeItem('microsoft-auth');
+
+        return user;
+      } else {
+        throw new Error(payload.message.text);
+      }
+    };
+
+    return Axios.put(GlobalConfig.uri.auth + 'logout', null).then(onSuccess);
+  }
 }
