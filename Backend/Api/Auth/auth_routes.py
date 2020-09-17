7f19df583defcd78fcc16ab24c706a5c718a1bee
Meharban Liaquat <meharban.liaquat@thetowertech.com>
2020-09-17 17:13:56 +0500

    updated setup urls

--- Backend/Api/Auth/auth_routes.py
+++ Backend/Api/Auth/auth_routes.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .authentication import *
 
 urlpatterns = [
-    # path('auth/authentication/login', LoginClass.as_view()),
-    # path('auth/authentication/register', RegisterClass.as_view()),
+    path('auth/authentication/login', LoginClass.as_view()),
+    path('auth/authentication/register', RegisterClass.as_view()),
 
 ]
