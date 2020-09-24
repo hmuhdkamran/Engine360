6ef76a975adbaa2f2a564b2d9d43a4a7bc636e1b
H.Muhammad Kamran <hmuhdkamran@gmail.com>
2020-09-24 19:45:35 +0500

    Update configuration

--- Backend/Engine/settings.py
+++ Backend/Engine/settings.py
@@ -100,8 +100,8 @@ Password = SettingInfoDic['Password']
 Database = SettingInfoDic['Database']
 Port = SettingInfoDic['Port']
 
-REDIS_HOST = ConfigLoad['redis']['HOST']
-REDIS_PORT = ConfigLoad['redis']['PORT']
+REDIS_HOST = ConfigLoad['redis']['redisHost']
+REDIS_PORT = ConfigLoad['redis']['redisPort']
 
 CACHES = {
     "default": {
@@ -110,7 +110,7 @@ CACHES = {
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
-        "KEY_PREFIX": "engine360"
+        "KEY_PREFIX": ConfigLoad['tokenProvider']['tokenAudience']
     }
 }
 
